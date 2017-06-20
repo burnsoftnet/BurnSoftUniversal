a1f5a18bb8daa6a37fdc763887a49aa0c04ee64f
Joe Mireles <bs.GitHub@burnsoft.net>
2017-06-19 21:59:21 -0400

    Updated methods and additional comments

--- BurnSoftUniversal/DBManager.h
+++ BurnSoftUniversal/DBManager.h
@@ -17,24 +17,31 @@
 #pragma mark Init Database with File name
 //Initialize the database with the file name
 -(instancetype)initWithDatabaseFileName:(NSString *)dbFilename;
+
 #pragma mark Load Data from DB into Array
 // Pass a STL statement to have the data loaded into an Array
 -(NSArray *)loadDataFromDB:(NSString *)query;
+
 #pragma mark Execute SQL statement
 //pass a sqlstatement to be executed, usually INSERT,UPDATE, DELETE, etc.
 -(void)executeQuery:(NSString *)query;
+
 #pragma mark Error Handling
 //NOTE: Translate Errors from SQLITE integer to English
 -(NSString *)dbErrors:(int) ret;
+
 #pragma mark Get Database Path
 //NOTE: Pass the Database Name to find the Path of the database
 -(NSString *) getDatabasePath :(NSString *) DBNAME;
+
 #pragma mark Copy DB if Needed
 //NOTE: Pass the name of the database to see if we need to copy the database from the application directory to the documents directory
 -(void) copyDbIfNeeded :(NSString *) DBNAME MessageHandler:(NSString **) msg;
+
 #pragma mark Check Database
 //NOTE: Pass the Database name to see if the database is in the path that we need it to be in
 -(void)checkDB :(NSString *) DBNAME MessageHandler:(NSString **) msg;
+
 #pragma mark Restory Factory Database
 //NOTE: Retore the Factory Database by deleting the database in the user docs and copying it back over.
 -(void) restoreFactoryDB :(NSString *) DBNAME MessageHandler:(NSString **) msg;
