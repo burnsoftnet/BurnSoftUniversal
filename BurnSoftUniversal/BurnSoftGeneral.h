f91df6206f5c4cd338f88676db1a826b06842683
Joe Mireles <bs.GitHub@burnsoft.net>
2017-08-13 21:09:00 -0400

    Deleted File System Functions since they were moved

--- BurnSoftUniversal/BurnSoftGeneral.h
+++ BurnSoftUniversal/BurnSoftGeneral.h
@@ -54,42 +54,4 @@
 // Get the Current Date and Time Stampe and have it returned in a connected format
 +(NSString *)formatLongConnectedDateTimeStamp;
 
-//TO DELETE - This was Moved to to BurnSoftFileSystem Class
-
-#pragma mark CopyFile
-// Simplify the copy and replace method with overwriteoption
-+(BOOL) copyFileFromFilePath:(NSString *) fromPath ToNewPath:(NSString *) toPath ErrorMessage:(NSString **) msg;
-
-#pragma mark Delete File byPath
-// Pass the path and file to delete that file
-+(BOOL)DeleteFileByPath:(NSString *) sPath ErrorMessage:(NSString **) msg;
-
-#pragma mark Load Files in Path by Extension
-//Load all the files in the target path that have a certain type of extension
-+(NSArray *) getCertainFilefromPath:(NSString *) sPath ByExtension:(NSString *) myExt;
-
-#pragma mark Load Files in Local Directory by Extension
-// Load all the files in the Local docuemtns directory by a certain extention
-+(NSArray *) getCertainFilesFromDocumentsByExtension:(NSString *) myExt;
-
-#pragma mark Return Full Path of App Documents with file name
-//Return Full Path of App Documents with file name attached
-+(NSString *) returnFullPathwithFileName:(NSString *) myFile;
-
-#pragma mark CopyFile 2
-// Simplify the copy and replace method with overwriteoption
-+(BOOL) copyFileFrom:(NSString *) sFrom To:(NSString *) sTo ErrorMessage:(NSString **) errorMessage;
-
-#pragma mark Create a Directory
-//Create a directory if it doesn't already exist
-+(BOOL)createDirectoryIfNotExists:(NSString *) sPath ErrorMessage:(NSString **) errMsg;
-
-#pragma mark Convert Bool to String
-//Convert a boolean value to string Yes or No
-+(NSString *) convertBOOLtoString:(BOOL) bValue;
-
-#pragma mark Get File Exteension From File Path
-// Get the extension of the file from the full path
-+(NSString *) getFileExtensionbyPath:(NSString *) filePath;
-
 @end
